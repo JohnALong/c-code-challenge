551bb2e9745a7d2567d2f85ade2dfcc8671832d0
John Long <jlcomp45@gmail.com>
2020-07-30 11:54:40 -0500

    returning list view of industries

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,35 @@ import React from "react";
 import IndustryList from "./IndustryList";
 import SelectedIndustries from "./SelectedIndustries";
 import SearchBar from "./SearchBar";
+import industries from "../apis/cAPI";
+import { Container, Col, Row } from "react-bootstrap";
 
 class App extends React.Component {
+  state = { industries: [] };
+
+  onLoad = async () => {
+    const response = await industries.get();
+    console.log("response", response.data);
+    this.setState({ industries: response.data });
+  };
+
+  componentDidMount() {
+    this.onLoad();
+  }
+
   render() {
     return (
-      <div>
+      <Container>
         <SearchBar />
-        <IndustryList />
-        <SelectedIndustries />
-      </div>
+        <Row>
+          <Col>
+            <IndustryList industries={this.state.industries} />
+          </Col>
+          <Col>
+            <SelectedIndustries />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
--- src/components/IndustryList.js
+++ src/components/IndustryList.js
@@ -1,7 +1,13 @@
 import React from "react";
+import IndustryItem from "./IndustryItem";
+import { render } from "@testing-library/react";
 
 const IndustryList = (props) => {
-  return <div>This will be the list</div>;
+  const renderedList = props.industries.map((industry) => {
+    return <IndustryItem industry={industry} />;
+  });
+  console.log("list props2", props);
+  return <div>{renderedList}</div>;
 };
 
 export default IndustryList;
