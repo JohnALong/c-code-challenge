6ada63b491162d331166769a3b709480f80668b2
John Long <jlcomp45@gmail.com>
2020-07-30 10:22:56 -0500

    created individual needed components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import IndustryList from "./IndustryList";
+import SelectedIndustries from "./SelectedIndustries";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
-    return <div>this is my app</div>;
+    return (
+      <div>
+        <SearchBar />
+        <IndustryList />
+        <SelectedIndustries />
+      </div>
+    );
   }
 }
 
