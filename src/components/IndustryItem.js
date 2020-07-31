feac3d3c3d21c3d0cf4c29144fb7071d47205152
John Long <jlcomp45@gmail.com>
2020-07-31 08:51:53 -0500

    search by string works - needs refactor to search by sic code

--- src/components/IndustryItem.js
+++ src/components/IndustryItem.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const IndustryItem = ({ industry }) => {
   //   console.log("item props", industry.title, industry.sic_code);
+  //   console.log("industry", industry);
   return (
     <div>
       <div>{industry.title}</div>
--- src/components/IndustryList.js
+++ src/components/IndustryList.js
@@ -1,13 +1,17 @@
 import React from "react";
 import IndustryItem from "./IndustryItem";
-import { render } from "@testing-library/react";
 
 const IndustryList = (props) => {
   const renderedList = props.industries.map((industry) => {
     return <IndustryItem industry={industry} />;
   });
   //   console.log("list props2", props);
-  return <div>{renderedList}</div>;
+  console.log("renderedList in list", renderedList);
+  if (renderedList.length != 0) {
+    return <div>{renderedList}</div>;
+  } else {
+    return <div>Please check your spelling</div>;
+  }
 };
 
 export default IndustryList;
